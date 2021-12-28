0fd6df1339fa5eba2705b7e8543c94569536dc6e
stebucko360 <stebucko360@gmail.com>
2021-12-28 22:01:44 +0000

    added spaces between player and : for clarity

--- noughtsandcrosses/src/components/Scoreboard.jsx
+++ noughtsandcrosses/src/components/Scoreboard.jsx
@@ -5,8 +5,8 @@ export const Scoreboard = ({playerOneScore, playerTwoScore}) => {
     
     return (
         <div className= 'scoreboard'>
-            <h3>Player 1: {playerOneScore}</h3>
-            <h3>Player 2: {playerTwoScore}</h3>
+            <h3>Player 1 : {playerOneScore}</h3>
+            <h3>Player 2 : {playerTwoScore}</h3>
         </div>
     )
 }
